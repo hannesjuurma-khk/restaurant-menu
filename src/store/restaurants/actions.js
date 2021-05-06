5f467bb79b57e41e4437b0e0e8fd9f65199ba985
hannesjuurma <55531505+hannesjuurma@users.noreply.github.com>
2021-05-06 12:15:28 +0300

    As an user i can add restaurants to the list [Delivers #178043602]

--- src/store/restaurants/actions.js
+++ src/store/restaurants/actions.js
@@ -1,12 +1,37 @@
+export const START_LOADING = 'START_LOADING';
 export const STORE_RESTAURANTS = 'STORE_RESTAURANTS';
+export const RECORD_LOADING_ERROR = 'RECORD_LOADING_ERROR';
+export const ADD_RESTAURANT = 'ADD_RESTAURANT';
 
 export const loadRestaurants = () => (dispatch, getState, api) => {
-  api.loadRestaurants().then(records => {
-    dispatch(storeRestaurants(records));
-  });
+  const startLoading = () => ({type: START_LOADING});
+
+  dispatch(startLoading());
+
+  api
+    .loadRestaurants()
+    .then(records => {
+      dispatch(storeRestaurants(records));
+    })
+    .catch(() => {
+      dispatch(recordLoadingError());
+    });
 
   const storeRestaurants = records => ({
     type: STORE_RESTAURANTS,
     records,
+  });
+
+  const recordLoadingError = () => ({type: RECORD_LOADING_ERROR});
+};
+
+export const createRestaurant = name => (dispatch, getState, api) => {
+  const addRestaurant = record => ({
+    type: ADD_RESTAURANT,
+    record,
+  });
+
+  return api.createRestaurant(name).then(record => {
+    dispatch(addRestaurant(record));
   });
 };
